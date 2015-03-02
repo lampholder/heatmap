8334145f7eb53f1f40f0eeb7d91404590cea8476
Thomas Lant <lampholder@gmail.com>
2015-03-02 13:44:11 +0000

    Neater structure

--- p2ll.py
+++ p2ll.py
@@ -1,21 +1,34 @@
 import sys, csv
-from postcode import parse_uk_postcode
+from lib.postcode import parse_uk_postcode
 
 outcodes = {}
+donations = {}
+populations = {}
 
 # 1,AB10,57.131086,-2.122482
-with open('postcodes.csv', 'rb') as csvfile:
+with open('data/postcodes.csv', 'rb') as csvfile:
     reader = csv.reader(csvfile)
     outcodes = {outcode: (lat, lng) for _, outcode, lat, lng in reader}
+    donation_totals = {outcode: 0 for outcode in outcodes}
 
 # arthur smith,389 chiswick High Road,w44al,YES,447890123123,5.00,SMS
-with open('data/good.csv', 'rb') as datafile:
+with open('input.csv', 'rb') as datafile:
     reader = csv.reader(datafile)
-    donations = [(parse_uk_postcode(postcode, strict=False)[0], tarrif) for (name, address, postcode, giftaid, msisdn, tarrif, method) in reader]
+    donations = [(parse_uk_postcode(postcode, strict=False)[0], int(float(tarrif))) for (name, address, postcode, giftaid, msisdn, tarrif, method) in reader]
+    for outcode, tarrif in donations:
+        if outcode in donation_totals:
+            donation_totals[outcode] += tarrif
+
+with open('data/2011_population.csv', 'rb') as csvfile:
+    reader = csv.reader(csvfile)
+    reader.next()
+    populations = {outcode: int(population) for outcode, population, men, women in reader}
 
 writer = csv.writer(sys.stdout)
 writer.writerow(['lat', 'lon', 'value'])
-for outcode, tarrif in donations:
-    if outcode in outcodes:
+for outcode, total in donation_totals.iteritems():
+    if total > 0:
         lat, lng = outcodes[outcode]
-        writer.writerow([lat, lng, 1])
+        writer.writerow([lat, lng, total])
+        #if outcode in populations:
+        #    writer.writerow([lat, lng, float(total) / populations[outcode]])
