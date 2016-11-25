ce52b384aea6a041a3fbe811828ed1dfd4591383
Tom Lant <lampholder@gmail.com>
2016-11-25 14:11:03 +0000

    Skip header row

--- p2ll.py
+++ p2ll.py
@@ -1,6 +1,8 @@
 import sys, csv
 from lib.postcode import parse_uk_postcode
 
+# Data came from here: http://www.nomisweb.co.uk/census/2011/ks101ew
+
 outcodes = {}
 donations = {}
 populations = {}
@@ -12,9 +14,11 @@ with open('data/postcodes.csv', 'rb') as csvfile:
     donation_totals = {outcode: 0 for outcode in outcodes}
 
 # arthur smith,389 chiswick High Road,w44al,YES,447890123123,5.00,SMS
+#name,address 1,postcode,taxclaimable,amount,msisdn,paymenttype
 with open('input.csv', 'rb') as datafile:
     reader = csv.reader(datafile)
-    donations = [(parse_uk_postcode(postcode, strict=False)[0], int(float(tarrif))) for (name, address, postcode, giftaid, msisdn, tarrif, method) in reader]
+    reader.next()
+    donations = [(parse_uk_postcode(postcode, strict=False)[0], int(float(tarrif))) for (name, address, postcode, giftaid, tarrif, msisdn, method) in reader]
     for outcode, tarrif in donations:
         if outcode in donation_totals:
             donation_totals[outcode] += tarrif
@@ -29,6 +33,6 @@ writer.writerow(['lat', 'lon', 'value'])
 for outcode, total in donation_totals.iteritems():
     if total > 0:
         lat, lng = outcodes[outcode]
-        writer.writerow([lat, lng, total])
-        #if outcode in populations:
-        #    writer.writerow([lat, lng, float(total) / populations[outcode]])
+        #writer.writerow([lat, lng, total])
+        if outcode in populations:
+            writer.writerow([lat, lng, float(total) / populations[outcode]])
